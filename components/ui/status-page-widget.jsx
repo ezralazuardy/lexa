097a69be53a878ab5bf73490dc41f3b733d32a70
Ezra Lazuardy <ezralazuardy@icloud.com>
2025-01-29 03:30:02 +0700

    feat: update

--- components/ui/status-page-widget.jsx
+++ components/ui/status-page-widget.jsx
@@ -5,15 +5,19 @@ export default function StatusPageWidget() {
     <>
       <Script
         src="https://cdnjs.cloudflare.com/polyfill/v3/polyfill.min.js"
-        strategy="beforeInteractive"
+        strategy="lazyOnload"
       />
       <Script
         src="https://unpkg.com/@webcomponents/webcomponentsjs@2.1.3/webcomponents-bundle.js"
-        strategy="beforeInteractive"
+        strategy="lazyOnload"
       />
       <Script
         src="https://unpkg.com/@statuspage/status-widget/dist/index.js"
-        strategy="beforeInteractive"
+        strategy="lazyOnload"
+      />
+      <Script
+        src="https://lexa.statuspage.io/embed/script.js"
+        strategy="lazyOnload"
       />
       <statuspage-widget src={process.env.NEXT_PUBLIC_STATUS_PAGE_URL} />
     </>
