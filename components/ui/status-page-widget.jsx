702d50f5b9b045ee0e143201b04450805625a71f
Ezra Lazuardy <ezralazuardy@icloud.com>
2025-01-31 02:32:31 +0700

    feat: update

--- components/ui/status-page-widget.jsx
+++ components/ui/status-page-widget.jsx
@@ -1,25 +1,98 @@
-import Script from "next/script";
+"use client";
+
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function StatusPageWidget() {
+  const [statusData, setStatusData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const statusPageUrl = process.env.NEXT_PUBLIC_STATUS_PAGE_URL ?? null;
+
+  useEffect(() => {
+    const fetchStatus = async () => {
+      try {
+        const response = await fetch(statusPageUrl);
+        if (!response.ok) {
+          throw new Error("Failed to fetch status data");
+        }
+        const data = await response.json();
+        setStatusData(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchStatus();
+  }, []);
+
+  if (!statusPageUrl) {
+    return <div></div>;
+  }
+
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center space-x-2 transition-opacity duration-100 hover:opacity-80">
+        Loading Systems Status...
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="flex items-center justify-center space-x-2 transition-opacity duration-100 hover:opacity-80">
+        <div className="relative">
+          <div
+            className={`h-3 w-3 rounded-full bg-neutral-500 animate-ping`}
+          ></div>
+          <div
+            className={`absolute top-0 h-3 w-3 rounded-full bg-neutral-500`}
+          ></div>
+        </div>
+        <p className="text-sm font-light text-neutral-600">
+          Unknown Systems Status
+        </p>
+      </div>
+    );
+  }
+
+  const hasIncidents = statusData.ongoing_incidents.length > 0;
+  const hasMaintenances =
+    statusData.in_progress_maintenances.length > 0 ||
+    statusData.scheduled_maintenances.length > 0;
+
+  let status = "operational";
+  let statusText = "All Systems Operational";
+  let badgeColor = "bg-green-500";
+
+  if (hasIncidents) {
+    status = "issues";
+    statusText = "Ongoing Issues Detected";
+    badgeColor = "bg-red-500";
+  } else if (hasMaintenances) {
+    status = "maintenance";
+    statusText = "Maintenance Scheduled";
+    badgeColor = "bg-yellow-500";
+  }
+
   return (
-    <>
-      <Script
-        src="https://cdnjs.cloudflare.com/polyfill/v3/polyfill.min.js"
-        strategy="lazyOnload"
-      />
-      <Script
-        src="https://unpkg.com/@webcomponents/webcomponentsjs@2.1.3/webcomponents-bundle.js"
-        strategy="lazyOnload"
-      />
-      <Script
-        src="https://unpkg.com/@statuspage/status-widget/dist/index.js"
-        strategy="lazyOnload"
-      />
-      <Script
-        src="https://lexa.statuspage.io/embed/script.js"
-        strategy="lazyOnload"
-      />
-      <statuspage-widget src={process.env.NEXT_PUBLIC_STATUS_PAGE_URL} />
-    </>
+    <Link
+      className="flex items-center justify-center space-x-2 transition-opacity duration-100 hover:opacity-80"
+      rel="noopener noreferrer"
+      target="_blank"
+      href={statusData.page_url}
+    >
+      <div className="relative">
+        <div
+          className={`h-3 w-3 rounded-full ${badgeColor} animate-ping`}
+        ></div>
+        <div
+          className={`absolute top-0 h-3 w-3 rounded-full ${badgeColor}`}
+        ></div>
+      </div>
+      <p className="text-sm font-light text-neutral-600">{statusText}</p>
+    </Link>
   );
 }
