3b0e59e42929644134ade1900ceb6ed40dbf41d7
Ezra Lazuardy <ezralazuardy@icloud.com>
2025-02-01 02:33:04 +0700

    feat: update

--- components/provider/vercel-provider.jsx
+++ components/provider/vercel-provider.jsx
@@ -3,16 +3,16 @@ import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function VercelProvider() {
-  if (isProductionMode()) {
-    return (
-      <>
-        {/* Vercel Analytics */}
-        <Analytics mode="production" />
-        {/* Vercel Speed Insights */}
-        <SpeedInsights />
-      </>
-    );
+  if (!isProductionMode()) {
+    return null;
   }
 
-  return null;
+  return (
+    <>
+      {/* Vercel Analytics */}
+      <Analytics mode="production" />
+      {/* Vercel Speed Insights */}
+      <SpeedInsights />
+    </>
+  );
 }
